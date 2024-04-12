4f3e7837480417f7c3d1fa5be5977d1c9892bc2d
Lucas Lu <lujingyu2919@gmail.com>
2024-04-13 03:39:01 +1000

    about section

---
+++ src/app/components/TabButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const variants = {
+  default: { width: 0 },
+  active: { width: 'calc(100% - 0.75rem)' }
+}
+
+const TabButton = ({ active, selectTab, children }) => {
+  const buttonClasses = active ? 'text-write' : 'text-[#ADB7BE]'
+
+  return (
+    <button onClick={selectTab}>
+      <p className={`mr-3 font-bold hover:text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-pink-400 ${buttonClasses}`}>
+        {children}
+      </p>
+      <motion.div
+        animate={active ? 'active' : 'default'}
+        variants={variants}
+        className='h-0.5 bg-gradient-to-r from-blue-500 to-pink-500 mt-2 mr-3'
+      ></motion.div>
+    </button>
+  )
+}
+
+export default TabButton
