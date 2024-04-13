6fe4d89b077cae8b43fdabc875099516803cf782
Lucas Lu <lujingyu2919@gmail.com>
2024-04-14 05:00:35 +1000

    MainSection.jsx

--- src/app/components/mainPage/MainSection.jsx
+++ src/app/components/mainPage/MainSection.jsx
@@ -71,9 +71,9 @@ const MainSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-transparent w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/avatar.png"
+              src="/images/Image.heic"
               alt="avatar image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={290}
