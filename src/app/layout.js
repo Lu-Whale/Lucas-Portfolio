3ad6a169ba7a66f333dd44cbf1915ea5df06bd5f
Lucas Lu <lujingyu2919@gmail.com>
2024-04-14 21:27:47 +1000

    website title and icon changed, dependencies updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,28 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import icon from '../../public/favicon.ico';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Jingyu (Lucas) Lu',
-  description: 'This is my personal portfolio',
+  description: 'This is my personal portfolio'
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='description' content={metadata.description} />
+        <title>{metadata.title}</title>
+        <link rel='icon' href={icon} type='image/x-icon' />
+      </Head>
+      <html lang='en'>
       <body className={inter.className}>{children}</body>
-    </html>
+      </html>
+    </>
   )
 }
