02b9c1bd9afbfed4b01f3293c5bafb03619b0f7c
Lucas Lu <lujingyu2919@gmail.com>
2024-04-24 05:22:38 +1000

    main section update on 4.24

--- src/app/components/mainPage/ResponsiveImage.jsx
+++ src/app/components/mainPage/ResponsiveImage.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 
 function ResponsiveImage() {
   //Set the initial state
   const [windowSize, setWindowSize] = useState({
     width: undefined,
-    height: undefined,
-  });
+    height: undefined
+  })
 
   useEffect(() => {
     // Handle the resizing action
@@ -14,31 +14,32 @@ function ResponsiveImage() {
       // Set the window size state
       setWindowSize({
         width: window.innerWidth,
-        height: window.innerHeight,
-      });
+        height: window.innerHeight
+      })
     }
-    // Listen for window resize events
-    window.addEventListener('resize', handleResize);
-    // Trigger the resize event to get initial values
-    handleResize();
-    // Clean up function
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
-  const imageWidth = windowSize.width >= 1024 ? 290 : 180;
-  const imageHeight = windowSize.width >= 1024 ? 290 : 180;
+    // Listen for window resize events
+    window.addEventListener('resize', handleResize)
+    // Trigger the resize event to get initial values
+    handleResize()
+    // Clean up function
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  const imageWidth = windowSize.width >= 1024 ? 290 : 180
+  const imageHeight = windowSize.width >= 1024 ? 290 : 180
 
   return (
-    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-      <Image
-        src="/images/avatar.png"
-        alt="avatar image"
-        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-        width={imageWidth}
-        height={imageHeight}
-      />
-    </div>
-  );
+    <Image
+      src='/images/avatar.png'
+      alt='avatar image'
+      className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-[#181818] relative animate-profile-animate'
+      width={imageWidth}
+      height={imageHeight}
+      layout='fixed'
+    />
+  )
 }
 
-export default ResponsiveImage;
+export default ResponsiveImage
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,21 @@ module.exports = {
         "gradient-conic":
             "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        profile_animate: {
+          '0%, 100%': {
+            borderRadius: '60% 40% 30% 70%/60% 30% 70% 40%',
+            boxShadow: 'inset 0 0 9px rgba(255, 255, 255, 0.3)', // adjust as necessary
+          },
+          '50%': {
+            borderRadius: '30% 60% 70% 40%/50% 60% 30% 60%',
+            boxShadow: 'inset 0 0 18px rgba(255, 255, 255, 0.3)', // adjust as necessary
+          },
+        },
+      },
+      animation: {
+        'profile-animate': 'profile_animate 8s ease-in-out infinite',
+      },
     },
     colors: {
       ...colors,
