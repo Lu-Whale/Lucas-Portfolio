93227ee9c56e8ca2a49a8b5d63d00a2579c4ac04
Lucas Lu <lujingyu2919@gmail.com>
2024-05-02 23:20:22 +1000

    project section updated, on 2 May

--- src/app/components/projects/ProjectCard.jsx
+++ src/app/components/projects/ProjectCard.jsx
@@ -4,27 +4,19 @@ import Link from 'next/link'
 
 const ProjectCard = ({ imgUrl, title, description, previewUrl }) => {
   return (
-    <div>
-      <div
-        className='h-52 md:h-72 rounded-t-xl relative group'
-        style={{ background: `url(${imgUrl})`, backgroundSize: 'cover' }}
-      >
+    <Link href={previewUrl}>
+      <div className='group cursor-pointer'>
         <div
-          className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 '>
-          <Link
-            href={previewUrl}
-            className='h-20 w-20 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'
-          >
-            <EyeIcon
-              className='h-16 w-16 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
-          </Link>
+          className='h-52 md:h-72 rounded-t-xl relative overflow-hidden transition-transform duration-300 ease-in-out hover:scale-110'
+          style={{ background: `url(${imgUrl})`, backgroundSize: 'cover' }}
+        >
+        </div>
+        <div className='text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4'>
+          <h5 className='text-xl text-center font-semibold mb-2'>{title}</h5>
+          <p className='text-[#ADB7BE]'>{description}</p>
         </div>
       </div>
-      <div className='text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4'>
-        <h5 className='text-xl text-center font-semibold mb-2'>{title}</h5>
-        <p className='text-[#ADB7BE]'>{description}</p>
-      </div>
-    </div>
+    </Link>
   )
 }
 
